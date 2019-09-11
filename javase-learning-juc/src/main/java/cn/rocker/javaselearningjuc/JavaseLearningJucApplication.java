d629f5846f48336ebea9cf5d0cb1256aa47dd107
chengzhichao <chengzhichao@le.com>
2019-09-11 19:00:57 +0800

    init

---
+++ javase-learning-juc/src/main/java/cn/rocker/javaselearningjuc/JavaseLearningJucApplication.java
@@ -0,0 +1,13 @@
+package cn.rocker.javaselearningjuc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaseLearningJucApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaseLearningJucApplication.class, args);
+    }
+
+}
---
+++ javase-learning-juc/src/test/java/cn/rocker/javaselearningjuc/JavaseLearningJucApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.rocker.javaselearningjuc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaseLearningJucApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
